45a9196926457650c865f07d9bd1280414d65cca
GuiSousa2910 <122701467+GuiSousa2910@users.noreply.github.com>
2024-09-07 16:28:47 -0300

    Subindo animacao cadastro

--- public/js/cadastro.js
+++ public/js/cadastro.js
@@ -1,22 +1,34 @@
 let i = 0;
 
 function changeRegister() {
-    var p_cadastro = document.getElementById("texto-cadastro");
     var cadastroEmpresa = document.getElementById("cadastro-empresa");
     var cadastroTeste = document.getElementById("cadastro-teste");
     var botao_mudar = document.getElementById("botao_registro");
 
     botao_mudar.innerHTML = "Cadastrar empresa";
-    cadastroTeste.style.transition = "opacity 0.5s ease, visibility 0.5s ease";
-    cadastroEmpresa.style.transition = "opacity 0.5s ease, visibility 0.5s ease";
 
+    // Remover o display de cadastroTeste e iniciar a transição
+    cadastroTeste.style.transition = "opacity 0.5s ease, visibility 0.5s ease";
     cadastroTeste.style.opacity = "0";
     cadastroTeste.style.visibility = "hidden";
-    cadastroTeste.style.display = "none";
 
-    cadastroEmpresa.style.display = "flex";
-    cadastroEmpresa.style.opacity = "1";
-    cadastroEmpresa.style.visibility = "visible";
+    // Esperar 500ms para alterar o display após a transição de opacidade
+    setTimeout(function () {
+        cadastroTeste.style.display = "none";
+        cadastroEmpresa.style.display = "flex";
+
+        // Iniciar a transição de opacidade e visibilidade do cadastroEmpresa
+        setTimeout(function () {
+            cadastroEmpresa.style.transition = "opacity 0.5s ease, visibility 0.5s ease";
+            cadastroEmpresa.style.opacity = "1";
+            cadastroEmpresa.style.visibility = "visible";
+        }, 10); 
+        setTimeout(function () {
+            cadastroTeste.style.transition = "opacity 0.5s ease, visibility 0.5s ease";
+            cadastroTeste.style.opacity = "1";
+            cadastroTeste.style.visibility = "visible";
+        }, 10);
+    }, 500);
 
     i++;
 }
