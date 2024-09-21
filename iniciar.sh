415f517f2b9935cea69af18e2af30cff52ab538e
Gustavo Gil <gustavo.gil@sptech.school>
2024-09-20 22:37:05 -0300

    cores no iniciar

--- iniciar.sh
+++ iniciar.sh
@@ -6,6 +6,9 @@ RED='\033[0;31m'    # Cor vermelha
 YELLOW='\033[1;33m' # Cor amarela
 NC='\033[0m'        # Sem cor (para resetar)
 
+echo -e "${YELLOW}Instalando Dependências NODE...${NC}"
 npm i
-
+echo -e "${GREEN}Dependências NODE Instaladas...${NC}"
+echo -e "${YELLOW}Iniciando Aplicação...${NC}"
 npm start
+echo -e "${GREEN}Aplicação Iniciada...${NC}"
