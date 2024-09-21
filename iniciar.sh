c899d08fb3afc0ed8b0892c64e5a266573040fbe
Gustavo Gil <gustavo.gil@sptech.school>
2024-09-20 22:18:45 -0300

    cores no terminal para melhor visualizacao

--- iniciar.sh
+++ iniciar.sh
@@ -6,6 +6,8 @@ RED='\033[0;31m'    # Cor vermelha
 YELLOW='\033[1;33m' # Cor amarela
 NC='\033[0m'        # Sem cor (para resetar)
 
+echo -e "${YELLOW}Instalando dependências do Node...${NC}"
 npm i
 
+echo -e "${YELLOW}Iniciando o servidor Node...${NC}"
 npm start
